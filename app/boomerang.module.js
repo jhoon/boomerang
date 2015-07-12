c66f83fc8cd45dc960cbbffa3136fbbc1955d446
Eduardo Medina <emedinaa@gmail.com>
2015-07-11 20:13:16 -0500

    link github

--- app/boomerang.module.js
+++ app/boomerang.module.js
@@ -7,6 +7,7 @@ angular.module('gdgXBoomerang', ['ngRoute', 'ngSanitize', 'ngAria', 'ngAnimate',
     mc.twitterLink = Config.twitter ? 'https://twitter.com/' + Config.twitter : null;
     mc.facebookLink = Config.facebook ? 'https://www.facebook.com/' + Config.facebook : null;
     mc.meetupLink = Config.meetup ? 'http://www.meetup.com/' + Config.meetup : null;
+    mc.githubLink = 'https://github.com/gdglima/';
     $rootScope.canonical = Config.domain;
 
     NavService.registerNavListener(function (tab) {
