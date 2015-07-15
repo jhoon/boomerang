fb1fd4467155659406dc2d1b277dacc12654847a
Eduardo Medina Alfaro <emedinaa@gmail.com>
2015-07-15 12:13:43 -0500

    correcciones

--- app/activities/activities.js
+++ app/activities/activities.js
@@ -8,39 +8,39 @@ angular.module('gdgXBoomerang')
     var activityList = {
         devFests: {
             name: 'Dev Fest Lima',
-            description: 'Dev Fest Lima es un evento a gran escala por la comunidad y para la comunidad . La temática  de los contenidos del evento gira, principalmente, entorno a tecnologias' +
+            description: 'Dev Fest Lima es un evento a gran escala por la comunidad y para la comunidad . Los contenidos del evento giran en torno a tecnologías ' +
                 'desarrolladas por Google',
             color: 'deepBlue',
             icon: '/app/images/icons/ic_event_48px.svg'
         },
         appClinics: {
             name: 'IO Extended Lima',
-            description: 'El Google I/O Extended Lima es un evento donde los desarrolladores puedan compartir y debatir de una forma más amena lo que se presentó en el Google I/O en Moscone Center',
+            description: 'El Google I/O Extended Lima es un evento donde los desarrolladores puedan compartir y debatir de una forma más amena lo presentado en el Google I/O',
             color: 'yellow',
             icon: '/app/images/icons/ic_local_hospital_48px.svg'
         },
         panels: {
             name: 'Study Jam Android',
-            description: 'Grupo de estudio relacionado con tencologia android',
+            description: 'Grupo de estudio relacionado a tecnologías Android',
             color: 'lightPurple',
             icon: '/app/images/icons/ic_people_48px.svg'
         },
 
         techTalks: {
             name: 'Meetup',
-            description: 'Conversatorio relacionados con tecnologia google ',
+            description: 'Conversatorio relacionados a tecnologías de Google',
             color: 'purple',
             icon: '/app/images/icons/ic_mic_48px.svg'
         },
         roundTables: {
             name: 'Coding Dojo',
-            description: 'Un coding dojo es una reunión de programadores que se juntan para practicar programación',
+            description: 'Un coding dojo es una reunión de programadores para practicar programación a través de ejercicios prácticos.',
             color: 'darkBlue',
             icon: '/app/images/icons/ic_local_pizza_48px.svg'
         },
         codeLabs: {
             name: 'GLabs',
-            description: 'Talleres prácticos relacionandos con tecnologia google',
+            description: 'Talleres prácticos relacionados a tecnologías de Google',
             color: 'green',
             icon: '/app/images/icons/ic_code_48px.svg'
         }
